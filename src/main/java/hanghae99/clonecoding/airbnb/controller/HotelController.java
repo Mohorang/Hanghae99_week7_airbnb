07f2549b92d1ffc4e9aebf119deff6fd87d8fbc0
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-06-21 20:33:50 +0900

    complete searchHotels api

--- src/main/java/hanghae99/clonecoding/airbnb/controller/HotelController.java
+++ src/main/java/hanghae99/clonecoding/airbnb/controller/HotelController.java
@@ -1,18 +1,19 @@
 package hanghae99.clonecoding.airbnb.controller;
 
 
+import hanghae99.clonecoding.airbnb.dto.RequestHotelsDto;
 import hanghae99.clonecoding.airbnb.dto.ResponseHotelDetailDto;
+import hanghae99.clonecoding.airbnb.dto.ResponseHotelsDto;
 import hanghae99.clonecoding.airbnb.dto.registerHotelDto;
-import hanghae99.clonecoding.airbnb.entity.Hotel;
 import hanghae99.clonecoding.airbnb.service.HotelService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,38 +24,6 @@ import java.util.Map;
 public class HotelController {
 
     private final HotelService service;
-
-
-    //숙소 등록
-    @PostMapping("/hotel")
-    public ResponseEntity registerHotel(@RequestPart(value = "mainImage") MultipartFile mainImage,
-                              @RequestPart(value = "images") List<MultipartFile> images,
-                              @RequestPart(value = "hotelData") registerHotelDto dto){
-        service.registerHotel(mainImage,images,dto);
-        return ResponseEntity.ok().body(HttpStatus.CREATED);
-    }
-
-    //숙소 수정
-    @PutMapping("/hotel/{id}")
-    public ResponseEntity modifyHotel(@PathVariable long id,
-                                      @RequestPart(value = "mainImage", required = false) MultipartFile mainImage,
-                                      @RequestPart(value = "images", required = false) List<MultipartFile> images,
-                                      @RequestPart(value = "hotelData") registerHotelDto dto){
-        service.modifyHotel(id,mainImage, images, dto);
-        return ResponseEntity.ok().body(HttpStatus.CREATED);
-    }
-
-    @DeleteMapping("/hotel/{id}")
-    public void deleteHotel(@PathVariable long id){
-        service.deleteHotel(id);
-    }
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
-    }
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler
-//    public
 
     // TODO: 2022/06/18
     // IllegalArgumentException 처리 handler
@@ -69,6 +38,45 @@ public class HotelController {
         return errorAttributes;
     }
 
+
+    //숙소 등록
+    @PostMapping("/hotel")
+    public ResponseEntity registerHotel(@RequestPart(value = "mainImage") MultipartFile mainImage,
+                                        @RequestPart(value = "images") List<MultipartFile> images,
+                                        @RequestPart(value = "hotelData") registerHotelDto dto) {
+        service.registerHotel(mainImage, images, dto);
+        return ResponseEntity.ok().body(HttpStatus.CREATED);
+    }
+
+    //숙소 수정
+    @PutMapping("/hotel/{id}")
+    public ResponseEntity modifyHotel(@PathVariable long id,
+                                      @RequestPart(value = "mainImage", required = false) MultipartFile mainImage,
+                                      @RequestPart(value = "images", required = false) List<MultipartFile> images,
+                                      @RequestPart(value = "hotelData") registerHotelDto dto) {
+        service.modifyHotel(id, mainImage, images, dto);
+        return ResponseEntity.ok().body(HttpStatus.CREATED);
+    }
+
+//    @DeleteMapping("/hotel/{id}")
+//    public void deleteHotel(@PathVariable long id){
+//        service.deleteHotel(id);
+//    }
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+//    }
+
+
+    // TODO: 2022/06/20
+    // 숙소 조회 ( 메인 페이지 )
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/hotels")
+    public ResponseHotelsDto searchHotels(@Valid RequestHotelsDto requestHotelsDto) {
+        return service.searchHotels(requestHotelsDto);
+    }
+
+
     // TODO: 2022/06/18
     // 숙소 상세 조회 API
     // IllegalArgumentException 처리 필요
@@ -78,7 +86,4 @@ public class HotelController {
     public ResponseHotelDetailDto searchHotelDetail(@PathVariable long id) {
         return service.searchHotelDetail(id);
     }
-
-    // PrincipalDetails : 임시 인가받은 유저 class
-
 }
